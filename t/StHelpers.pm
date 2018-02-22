a0b33a3101948c2283951622b0ed3e70662f783e
Vincent Tavernier <vince.tavernier@gmail.com>
2018-02-22 17:21:08 +0100

    Fix output capturing in tests

--- t/StHelpers.pm
+++ t/StHelpers.pm
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 use Test::More;
 use File::Spec;
+use IPC::Open3;
 require Exporter;
 
 our @ISA = qw(Exporter);
@@ -17,53 +18,78 @@ sub binary_path {
 	# else, undefined: use installed version
 }
 
+sub command_run {
+	my ($cmd, $code, $message) = @_;
+	# Print the code to be executed
+	SKIP: {
+		eval {
+			# Start process
+			my $pid = open3(\*CHLD_IN, \*CHLD_OUT, \*CHLD_OUT,
+				$cmd);
+
+			# Send code
+			print CHLD_IN $code;
+
+			# Get output
+			my $output = do { local $/; <CHLD_OUT> };
+
+			# Wait for exit
+			waitpid($pid, 0);
+			my $exit_status = $? >> 8;
+
+			# Print code details
+			diag "Running the following code in $cmd: ";
+			$code =~ s/^/  /gm;
+			diag $code;
+			diag "Command output: ";
+			$output =~ s/^/  /gm;
+			diag $output;
+
+			# Print output
+			is($exit_status, 0, $message);
+		};
+
+		skip("$message (skipped because $@)", 1) if $@;
+	}
+}
+
 sub octave_ok {
 	my ($message, $code) = @_;
-	SKIP: {
-		if (defined binary_path) {
-			# Append code to load the current testing version
-			my $path = File::Spec->catfile(binary_path, 'shadertoy_octave.oct');
-			$code = <<OCTAVE_CODE;
+	$message = "Octave: $message";
+
+	# Append code to load the current testing version
+	if (defined binary_path) {
+		my $path = File::Spec->catfile(binary_path, 'shadertoy_octave.oct');
+		$code = <<OCTAVE_CODE;
 autoload("shadertoy_octave", "$path");
 shadertoy_octave();
 $code
 OCTAVE_CODE
-		} else {
-			# Just load the library
-			$code = <<OCTAVE_CODE;
+	} else {
+		# Just load the library
+		$code = <<OCTAVE_CODE;
 shadertoy_octave();
 $code
 OCTAVE_CODE
-		}
-		# Print the code to be executed
-		diag $code;
-		# Start Octave process
-		open my $proc, '|octave-cli' or skip("$message (skipped because octave-cli is missing)", 1);
-		# Forward code
-		print $proc $code;
-		# Wait for exit and build test
-		ok(close($proc), $message);
 	}
+
+	command_run 'octave-cli', $code, $message;
 }
 
 sub mathematica_ok {
 	my ($message, $code) = @_;
-	SKIP: {
-		$code = <<MATHEMATICA_CODE;
+	$message = "Mathematica: $message";
+
+	# Append exit wrapper
+	$code = <<MATHEMATICA_CODE;
 On[Assert];
 Exit[If[FailureQ[Check[Module[{},
 <<Shadertoy`;
 $code],\$Failed]],1,0]]
 MATHEMATICA_CODE
-		$code =~ s/;(?!\n)/;\n/g;
-		diag $code;
-		# Start Mathematica process
-		open my $proc, '|math' or skip("$message (skipped because math is missing)", 1);
-		# Forward code
-		print $proc $code;
-		# Wait for exit and build test
-		ok(close($proc), $message);
-	}
+	$code =~ s/;(?!\n)/;\n/g;
+
+	command_run 'math', $code, $message;
 }
 
 1;
