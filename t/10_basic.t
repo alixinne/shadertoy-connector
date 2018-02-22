437024d8c9fd55324b1ec0b079a9e782ae4d7de1
Vincent Tavernier <vince.tavernier@gmail.com>
2018-02-22 12:40:55 +0100

    Add Mathematica support to testing

--- t/10_basic.t
+++ t/10_basic.t
@@ -4,11 +4,23 @@ use warnings;
 use FindBin;
 use lib "$FindBin::Bin/";
 use StHelpers;
-use Test::Simple tests => 1;
+use Test::More tests => 2;
 
-octave_ok 'Octave basic shader', <<OCTAVE_CODE
+my $shader = <<GLSL;
+void mainImage(out vec4 O, in vec2 U){O=vec4(iFrame, iResolution.x, U.xy);}
+GLSL
+$shader =~ s/\n//g;
+
+octave_ok 'Basic shader', <<OCTAVE_CODE;
 shadertoy_octave();
-ctxt = st_compile('void mainImage(out vec4 O, in vec2 U){O=vec4(iFrame, iResolution.x, U.xy);}');
+ctxt = st_compile("$shader");
 img = st_render(ctxt, 0, 'RGBA', 2, 2);
 exit(ifelse(all(diag(img(1,1,:)(:) == [0.0 2.0 0.5 1.5])),0,2))
 OCTAVE_CODE
+
+mathematica_ok 'Basic shader', <<MATHEMATICA_CODE;
+<<Shadertoy`;
+ctxt = CompileShadertoy["$shader"];
+img = RenderShadertoy[ctxt, Format -> "RGBA", Frame -> 0, Size -> { 2, 2 }];
+Exit[If[img[[1, 1]] == {0.0, 2.0, 0.5, 1.5},0,1]];
+MATHEMATICA_CODE
--- t/StHelpers.pm
+++ t/StHelpers.pm
@@ -2,20 +2,36 @@ package StHelpers;
 
 use strict;
 use warnings;
-require Test::Simple;
+use Test::More;
 require Exporter;
 
 our @ISA = qw(Exporter);
-our @EXPORT = qw(octave_ok);
+our @EXPORT = qw(octave_ok mathematica_ok);
 
 sub octave_ok {
 	my ($message, $code) = @_;
-	# Start Octave process
-	open my $proc, '|octave-cli';
-	# Forward code
-	print $proc $code;
-	# Wait for exit and build test
-	ok(close($proc), $message);
+	diag $code;
+	SKIP: {
+		# Start Octave process
+		open my $proc, '|octave-cli' or skip($message, 1);
+		# Forward code
+		print $proc $code;
+		# Wait for exit and build test
+		ok(close($proc), $message);
+	}
+}
+
+sub mathematica_ok {
+	my ($message, $code) = @_;
+	diag $code;
+	SKIP: {
+		# Start Mathematica process
+		open my $proc, '|math' or skip($message, 1);
+		# Forward code
+		print $proc $code;
+		# Wait for exit and build test
+		ok(close($proc), $message);
+	}
 }
 
 1;
