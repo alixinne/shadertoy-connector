2878329a7d5b4cb9d0e489dd8af9cffa0e82ec71
Vincent Tavernier <vince.tavernier@gmail.com>
2018-05-17 02:05:17 +0200

    Add --debug to shadertoy_server

--- src/cli/server.cpp
+++ src/cli/server.cpp
@@ -1,13 +1,45 @@
+#include <iostream>
+
 #include "server.hpp"
 
 #include <shadertoy/spdlog/spdlog.h>
 
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
+
 int main(int argc, char *argv[])
 {
-	spdlog::set_level(spdlog::level::debug);
+	bool debug_mode;
 
-	server srv("tcp://*:13710");
-	srv.run();
+	try
+	{
+		po::options_description desc("shadertoy-connector server");
+		desc.add_options()
+			("help,h", "Show this help message")
+			("debug", po::bool_switch(&debug_mode)->default_value(false), "Enable debug output");
+
+		po::variables_map vm;
+		po::store(po::parse_command_line(argc, argv, desc), vm);
+		po::notify(vm);
+
+		if (vm.count("help"))
+			std::cout << desc << std::endl;
+		else
+		{
+			if (debug_mode)
+				spdlog::set_level(spdlog::level::debug);
+
+			server srv("tcp://*:13710");
+			srv.run();
+		}
+	}
+	catch (const po::error &ex)
+	{
+		std::cerr << ex.what() << std::endl;
+		return 1;
+	}
+
 	return 0;
 }
 
