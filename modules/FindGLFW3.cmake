1b6204f265b95c1cf8fb7190a41a6adf6368dd77
Vincent Tavernier <vince.tavernier@gmail.com>
2018-05-28 02:17:00 +0200

    Add find modules

---
+++ modules/FindGLFW3.cmake
@@ -0,0 +1,24 @@
+
+# Find package for glfw3
+include(LibFindMacros)
+
+# Use pkg-config to get hints about paths
+libfind_pkg_check_modules(glfw3_PKGCONF glfw3)
+
+# Use find_package
+find_package(glfw3 CONFIG QUIET)
+
+# Include directory
+find_path(GLFW3_INCLUDE_DIR_FIND
+	NAMES GLFW/glfw3.h
+	PATHS ${glfw3_PKGCONF_INCLUDE_DIRS} ${glfw3_INCLUDE_DIRS})
+
+# Library
+find_library(GLFW3_LIBRARY_FIND
+	NAMES glfw3 glfw3dll
+	PATHS ${glfw3_PKGCONF_LIBRARY_DIRS} ${glfw3_LIBRARY_DIRS})
+
+# Set include dir and libraries variables
+set(GLFW3_PROCESS_INCLUDES GLFW3_INCLUDE_DIR_FIND)
+set(GLFW3_PROCESS_LIBS GLFW3_LIBRARY_FIND)
+libfind_process(GLFW3)
---
+++ modules/Findjsoncpp.cmake
@@ -0,0 +1,20 @@
+# Find package for jsoncpp
+include(LibFindMacros)
+
+# Use pkg-config to get hints about paths
+libfind_pkg_check_modules(jsoncpp_PKGCONF jsoncpp)
+
+# Include directory
+find_path(jsoncpp_INCLUDE_DIR
+	NAMES json/json.h
+	PATHS ${jsoncpp_PKGCONF_INCLUDE_DIRS})
+
+# Library
+find_library(jsoncpp_LIBRARY
+	NAMES jsoncpp
+	PATHS ${jsoncpp_PKGCONF_LIBRARY_DIRS})
+
+# Set include dir and libraries variables
+set(jsoncpp_PROCESS_INCLUDES jsoncpp_INCLUDE_DIR)
+set(jsoncpp_PROCESS_LIBS jsoncpp_LIBRARY)
+libfind_process(jsoncpp)
